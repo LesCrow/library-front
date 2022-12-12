f3d9617e8b538179a7bc1cc12cfa3f5116447b9f
LesCrow <john.baruteau@gmail.com>
2022-12-12 22:52:08 +0100

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h1 className="py-10 text-red-500 text-center text-3xl">
+        Welcome to the Wild Library{" "}
+      </h1>
+    </div>
+  );
+}
+
+export default Header;
